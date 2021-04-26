1113590314cffbd0dce5f7ea01d14ec92c5c98aa
cptvictor95 <cpt.victor@hotmail.com>
2021-04-26 13:00:45 -0300

    start repo and create basic server structure

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { AddressInfo } from 'node:net';
+import app from './app';
+
+const PORT = process.env.PORT || 3003;
+
+const server = app.listen(PORT, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.info(`Server is running on port: ${PORT}`);
+  } else {
+    console.error('Failed running the server.');
+  }
+});
