3982e38b5032f03342b7c0ea0eaea787747bc2ea
cptvictor95 <cpt.victor@hotmail.com>
2021-04-27 16:35:06 -0300

    add comments and tests, update signUp validation, fix tsconfig.json

---
+++ build/data/tableNames.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.songTable = exports.userTable = void 0;
+exports.userTable = 'Users';
+exports.songTable = 'Songs';
+//# sourceMappingURL=tableNames.js.map
