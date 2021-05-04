46a012530ed2c34540569d206adaefe0557d422b
cptvictor95 <cpt.victor@hotmail.com>
2021-05-04 02:08:48 -0300

    last try

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,17 @@ const options: cors.CorsOptions = {
 };
 
 app.use(cors(options));
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://skibiddy-front.herokuapp.com'
+  ); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(express.json());
 
 // Routes
