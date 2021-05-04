0ed8ae17fb68969d7604893839ee1bfffc285174
cptvictor95 <cpt.victor@hotmail.com>
2021-05-04 02:16:52 -0300

    set cors origin to anywhere

--- src/app.ts
+++ src/app.ts
@@ -21,18 +21,7 @@ const options: cors.CorsOptions = {
   origin: ['http://localhost:3000', 'https://skibiddy-front.herokuapp.com'],
 };
 
-app.use(cors(options));
-app.use(function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://skibiddy-front.herokuapp.com'
-  ); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Routes
