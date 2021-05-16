61f6b5ae6b7126f30e37f71e692f1d09ca626a2d
cptvictor95 <cpt.victor@hotmail.com>
2021-05-16 01:44:45 -0300

    update filter validation

--- src/controller/song/getFilteredSongs.ts
+++ src/controller/song/getFilteredSongs.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import { getFilteredSongsBiz } from '../../business/song/getFilteredSongsBiz';
+import getSongsBiz from '../../business/song/getSongsBiz';
 import queryFilteredSongs from '../../data/song/queryFilteredSongs';
 import queryUserByName from '../../data/user/queryUserByName';
+import { authData } from '../../model/authData';
+import { getTokenData } from '../../services/auth';
 
 export const getFilteredSongs = async (
   req: Request,
@@ -11,17 +14,23 @@ export const getFilteredSongs = async (
     const { name, title, album, genre } = req.query;
 
     let user;
+    let songs;
     if (name) user = await queryUserByName(name as string);
     const token = req.headers.authorization as string;
+    const tokenData = getTokenData(token) as authData;
 
-    const songs = await getFilteredSongsBiz(
-      token,
-      queryFilteredSongs,
-      title as string,
-      album as string,
-      genre as string,
-      user?.id as string
-    );
+    if (name !== '' || title !== '' || album !== '' || genre !== '') {
+      songs = await getFilteredSongsBiz(
+        token,
+        queryFilteredSongs,
+        title as string,
+        album as string,
+        genre as string,
+        user?.id as string
+      );
+    } else {
+      songs = await getSongsBiz(tokenData);
+    }
 
     return res.status(200).send({
       status: 'Success!',
